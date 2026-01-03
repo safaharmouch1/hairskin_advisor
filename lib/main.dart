c9d224f6417c4c6fd5ab64c2a28cb1962141c833
Rayan Tayba <ghinatayba18@gmail.com>
2026-01-03 20:44:32 +0200

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'auth_page.dart';
+import 'home_page.dart';
 import 'history_page.dart';
-import 'main.dart' 
-;
 
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Future<String> _startRoute() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getInt("user_id");
+    return (userId == null) ? "/auth" : "/home";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +24,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         "/auth": (_) => const AuthPage(),
-        "/home": (_) => const MyApp(), // ✅ بدّلي HomePage باسم صفحتك
+        "/home": (_) => const HomePage(),
         "/history": (_) => const HistoryPage(),
       },
-       home: FutureBuilder<String>(
+      home: FutureBuilder<String>(
         future: _startRoute(),
         builder: (context, snap) {
           if (!snap.hasData) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
+            return const Scaffold(body: Center(child: CircularProgressIndicator()));
           }
           WidgetsBinding.instance.addPostFrameCallback((_) {
             Navigator.pushReplacementNamed(context, snap.data!);
