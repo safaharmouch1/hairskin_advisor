bbf4709053a551ea6e4adab69efc7b73f1751aad
Rayan Tayba <ghinatayba18@gmail.com>
2026-01-03 21:03:18 +0200

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -19,26 +19,58 @@ class MyApp extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        "/auth": (_) => const AuthPage(),
-        "/home": (_) => const HomePage(),
-        "/history": (_) => const HistoryPage(),
-      },
-      home: FutureBuilder<String>(
-        future: _startRoute(),
-        builder: (context, snap) {
-          if (!snap.hasData) {
-            return const Scaffold(body: Center(child: CircularProgressIndicator()));
-          }
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            Navigator.pushReplacementNamed(context, snap.data!);
-          });
-          return const SizedBox.shrink();
-        },
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+
+   
+    theme: ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xFFE91E63), 
       ),
-    );
-  }
+      scaffoldBackgroundColor: const Color(0xFFFFF6FA), 
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color(0xFFFFE4F1),
+        foregroundColor: Colors.black,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: const Color(0xFFFFF1F8),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 14),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14),
+          ),
+        ),
+      ),
+    ),
+
+    routes: {
+      "/auth": (_) => const AuthPage(),
+      "/home": (_) => const HomePage(),
+      "/history": (_) => const HistoryPage(),
+    },
+    home: FutureBuilder<String>(
+      future: _startRoute(),
+      builder: (context, snap) {
+        if (!snap.hasData) {
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          Navigator.pushReplacementNamed(context, snap.data!);
+        });
+        return const SizedBox.shrink();
+      },
+    ),
+  );
+}
+
 }
