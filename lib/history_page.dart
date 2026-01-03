3b4e73ca0c5ae61d672e89d5776ea5971d54a486
Rayan Tayba <ghinatayba18@gmail.com>
2026-01-03 20:35:26 +0200

    add history file

---
+++ lib/history_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'services/session.dart';
+import 'services/api_client.dart';
+
+class HistoryPage extends StatefulWidget {
+  const HistoryPage({super.key});
+
+  @override
+  State<HistoryPage> createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
